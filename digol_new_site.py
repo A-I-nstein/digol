f46be2d37777858b305e662e65475c865bb6d815
Anuranjan M B <anuranjan.m.b@gmail.com>
2023-07-27 19:26:23 +0530

    Update digol_new_site.py

--- digol_new_site.py
+++ digol_new_site.py
@@ -1,90 +1,249 @@
 #####-----importing neccessary libraries-----#####
 
-import os
-import requests
-import pinecone
-import numpy as np
-import pandas as pd
-from math import sqrt
-import tensorflow_hub as hub
-from bs4 import BeautifulSoup
+import imutils
+import streamlit as st
+from digol_backend import *
 
-pinecone.init(
-    api_key = os.environ.get("PINECONE_API_KEY"),
-    environment = os.environ.get("PINECONE_ENVIRONMENT")
+#####-----UI Elements-----#####
+
+# configuring site
+
+st.set_page_config(layout="wide", page_title="Digital Olfaction")
+
+hide_streamlit_style = """
+                   <style>
+                   #MainMenu {visibility: hidden;}
+                   footer {visibility: hidden;}
+                   </style>
+                   """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+# title page
+
+st.markdown('<h1 style = " font-size: 50px; text-align:center; font-weight:bold">Fragrance Recommendation System</h1>', unsafe_allow_html=True)
+st.markdown('<h1 style = " font-size: 25px; text-align:center; font-weight:bold">A Tool for Digital Olfaction</h1>', unsafe_allow_html=True)
+st.text("")
+
+# configuring sidebar
+
+method = st.sidebar.radio(
+    "Suggest fragrances based on?",
+    ("Fragrance", "Base notes")
 )
-index = pinecone.Index(os.environ.get("index_name"))
-model = hub.KerasLayer("https://tfhub.dev/google/nnlm-en-dim128/2")
 
-#####-----Backend Functions-----#####
 
-def to_float(n):
-  return float(n)
 
-def get_top_n(query, n):
-    embedding = model(list(query))
-    embedding = list(map(to_float, list((np.array(embedding[0])))))
-    result = index.query([embedding], top_k=n, include_metadata=True)
-    return result
+# configuring main page
 
-def load_base_notes():
-    notes_data = pd.read_csv('processed_data/base_notes.csv')
-    base_notes = []
-    for i in notes_data['base_notes']:
-        base_notes.append(i)
-    return base_notes
+base_notes = load_base_notes()
+fragrances = load_fragrances()
 
-def load_fragrances():
-    fragrances_data = pd.read_csv('processed_data/title.csv')
-    fragrances = []
-    for i in fragrances_data['title']:
-        fragrances.append(i)
-    return fragrances
+fragrance_page = st.container()
+base_notes_page = st.container()
 
-def find_link(brand, title):
-    url = 'https://www.google.com/search?q=' + brand+' '+title + '&tbm=isch&source=hp&biw=2&bih=2&ei=DKZaYfDjBorN1sQP1J6LsA4&ved=0ahUKEwjwmbmBl7DzAhWKppUCHVTPAuYQ4dUDCAc&uact=400&oq=images&gs_lcp=CgNpbWcQAzIICAAQgAQQsQMyCAgAEIAEELEDMggIABCABBCxAzIICAAQgAQQsQMyCAgAEIAEELEDMggIABCABBCxAzIICAAQgAQQsQMyCAgAEIAEELEDMgsIABCABBCxAxCDATIFCAAQgAQ6CAgAELEDEIMBUIEgWNImYOspaABwAHgAgAFViAHGA5IBATaYAQCgAQGqAQtnd3Mtd2l6LWltZ7ABAA&sclient=img'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    image_links = soup.find_all('img')
-    for tag in image_links:
-        link = tag.get('src')
-        if(link != None and  link[0:4] == 'http'):
-            return(link)
+#####-----suggest fragrances based on a fragrance-----#####
 
-def base_notes_to_fragrance(base_notes, gender, age):
-    query = ''.join(base_notes) + ' ' + gender + ' ' + age
-    suggested_fragrances = {}
-    result = get_top_n(query, 3)
-    count = 0
-    for record in result['matches']:
-        suggested_fragrances[count] = [
-                                        record['metadata']['title'],
-                                        record['metadata']['accords'],
-                                        record['metadata']['secondary notes'],
-                                        record['metadata']['gender'],
-                                        record['metadata']['age'],
-                                        find_link(record['metadata']['brand'], record['metadata']['title']),
-                                        record['metadata']['brand']
-                                        ]
-        count = count+1
-    return (suggested_fragrances)
+if (method == "Fragrance"):
 
-def fragrance_to_fragrance(fragrance):
-    perfumes = pd.read_csv('processed_data/molecule_perfume_final_with_sec_notes.csv')
-    data = perfumes.loc[perfumes['title']==fragrance]
-    query = data["age"] + " " + data["gender"] + " " + data["accords"] + " " + data["secondary notes"]
-    result = get_top_n(query, 3)
-    suggested_fragrances = {}
-    count = 0
-    for record in result['matches']:
-        suggested_fragrances[count] = [
-                                        record['metadata']['title'],
-                                        record['metadata']['accords'],
-                                        record['metadata']['secondary notes'],
-                                        record['metadata']['gender'],
-                                        record['metadata']['age'],
-                                        find_link(record['metadata']['brand'], record['metadata']['title']),
-                                        record['metadata']['brand']
-                                        ]
-        count = count+1
-    return (suggested_fragrances)
+    with fragrance_page:
+        st.markdown('<h1 style = " font-size: 40px; text-align:center; font-weight:bold">Find fragrances based on a fragrance</h1>', unsafe_allow_html=True)
+        st.markdown("""---""")
+        selected_fragrance = st.selectbox('Select your favourite fragrance', fragrances)
+        st.write("")
+        predict = st.button("Find similar Fragrances")
+        st.write("")
+
+        if predict:
+            progress_bar = st.progress(0)
+
+            st.markdown("""---""")
+            st.markdown('<h1 style = " font-size: 40px; text-align:center; font-weight:bold">Similar Fragrances</h1>', unsafe_allow_html=True)
+            st.markdown("""---""")
+            suggested_fragrances = fragrance_to_fragrance(selected_fragrance)
+            progress_bar.progress(50)
+
+            fragrance_1 = st.container()
+            st.markdown("""---""")
+            fragrance_2 = st.container()
+            st.markdown("""---""")
+            fragrance_3 = st.container()
+
+            progress_bar.progress(60)
+
+            with fragrance_1:
+                col1, col2, col3 = st.columns([2, 1, 3])
+                title = col1.container()
+                image = col2.container()
+                details = col3.container()
+                try:
+                    with title:
+                        st.subheader(suggested_fragrances[0][0])
+                    with image:
+                        st.image(imutils.url_to_image(suggested_fragrances[0][5]))
+                    with details:
+                        st.write("**BRAND:** " + suggested_fragrances[0][6])
+                        st.write("**BASE NOTES:** " + suggested_fragrances[0][1])
+                        st.write("**SECONDARY NOTES:** " + suggested_fragrances[0][2])
+                        st.write("**GENDER:** " + suggested_fragrances[0][3])
+                        st.write("**AGE:** " + suggested_fragrances[0][4])
+                except Exception as e:
+                    print(e)
+                    st.error("Could not load fragrance")
+
+            with fragrance_2:
+                col1, col2, col3 = st.columns([2, 1, 3])
+                title = col1.container()
+                image = col2.container()
+                details = col3.container()
+                try:
+                    with title:
+                        st.subheader(suggested_fragrances[1][0])
+                    with image:
+                        st.image(imutils.url_to_image(suggested_fragrances[1][5]))
+                    with details:
+                        st.write("**BRAND:** " + suggested_fragrances[1][6])
+                        st.write("**BASE NOTES:** " + suggested_fragrances[1][1])
+                        st.write("**SECONDARY NOTES:** " + suggested_fragrances[1][2])
+                        st.write("**GENDER:** " + suggested_fragrances[1][3])
+                        st.write("**AGE:** " + suggested_fragrances[1][4])
+                except Exception as e:
+                    print(e)
+                    st.error("Could not load fragrance")
+
+            with fragrance_3:
+                col1, col2, col3 = st.columns([2, 1, 3])
+                title = col1.container()
+                image = col2.container()
+                details = col3.container()
+                try:
+                    with title:
+                        st.subheader(suggested_fragrances[2][0])
+                    with image:
+                        st.image(imutils.url_to_image(suggested_fragrances[2][5]))
+                    with details:
+                        st.write("**BRAND:** " + suggested_fragrances[2][6])
+                        st.write("**BASE NOTES:** " + suggested_fragrances[2][1])
+                        st.write("**SECONDARY NOTES:** " + suggested_fragrances[2][2])
+                        st.write("**GENDER:** " + suggested_fragrances[2][3])
+                        st.write("**AGE:** " + suggested_fragrances[2][4])
+                except Exception as e:
+                    print(e)
+                    st.error("Could not load fragrance")
+
+            progress_bar.progress(100)
+
+#####-----suggest fragrances based on base notes-----#####
+
+else:
+
+    with base_notes_page:
+        st.markdown('<h1 style = " font-size: 40px; text-align:center; font-weight:bold">Find fragrances based on base notes</h1>', unsafe_allow_html=True)
+        st.markdown("""---""")
+        # configuring user data collection page
+
+        col1, col2 = st.columns(2)
+        gender_container = col1.container()
+        age_container = col2.container()
+
+        with gender_container:
+            st.subheader("Gender")
+            gender = st.radio(
+                "Suggest fragrances for",
+                ("Male", "Female", "Unisex")
+            )
+
+        with age_container:
+            st.subheader("Age")
+            age = st.radio(
+                "Suggest fragrances for people",
+                ("< 25", "> 25")
+            )
+
+        st.write("")
+        st.subheader("Base Notes")
+        selected_base_notes = st.multiselect('Select a minimum of 1 base note', base_notes)
+        st.write("")
+        predict = st.button("Find similar Fragrances")
+        st.write("")
+
+        if predict:
+            if len(selected_base_notes)<1:
+                st.error("Please select a minimum of 1 base note")
+            else:
+                progress_bar = st.progress(0)
+
+                st.markdown("""---""")
+                st.markdown('<h1 style = " font-size: 40px; text-align:center; font-weight:bold">Similar Fragrances</h1>', unsafe_allow_html=True)
+                st.markdown("""---""")
+                suggested_fragrances = base_notes_to_fragrance(selected_base_notes, gender, age)
+                progress_bar.progress(50)
+
+                fragrance_1 = st.container()
+                st.markdown("""---""")
+                fragrance_2 = st.container()
+                st.markdown("""---""")
+                fragrance_3 = st.container()
+
+                progress_bar.progress(60)
+
+                with fragrance_1:
+                    col1, col2, col3 = st.columns([2, 1, 3])
+                    title = col1.container()
+                    image = col2.container()
+                    details = col3.container()
+                    try:
+                        with title:
+                            st.subheader(suggested_fragrances[0][0])
+                        with image:
+                            st.image(imutils.url_to_image(suggested_fragrances[0][5]))
+                        with details:
+                            st.write("**BRAND:** " + suggested_fragrances[0][6])
+                            st.write("**BASE NOTES:** " + suggested_fragrances[0][1])
+                            st.write("**SECONDARY NOTES:** " + suggested_fragrances[0][2])
+                            st.write("**GENDER:** " + suggested_fragrances[0][3])
+                            st.write("**AGE:** " + suggested_fragrances[0][4])
+                    except Exception as e:
+                        print(e)
+                        st.error("Could not load fragrance")
+
+                with fragrance_2:
+                    col1, col2, col3 = st.columns([2, 1, 3])
+                    title = col1.container()
+                    image = col2.container()
+                    details = col3.container()
+                    try:
+                        with title:
+                            st.subheader(suggested_fragrances[1][0])
+                        with image:
+                            st.image(imutils.url_to_image(suggested_fragrances[1][5]))
+                        with details:
+                            st.write("**BRAND:** " + suggested_fragrances[1][6])
+                            st.write("**BASE NOTES:** " + suggested_fragrances[1][1])
+                            st.write("**SECONDARY NOTES:** " + suggested_fragrances[1][2])
+                            st.write("**GENDER:** " + suggested_fragrances[1][3])
+                            st.write("**AGE:** " + suggested_fragrances[1][4])
+                    except Exception as e:
+                        print(e)
+                        st.error("Could not load fragrance")
+
+                with fragrance_3:
+                    col1, col2, col3 = st.columns([2, 1, 3])
+                    title = col1.container()
+                    image = col2.container()
+                    details = col3.container()
+                    try:
+                        with title:
+                            st.subheader(suggested_fragrances[2][0])
+                        with image:
+                            st.image(imutils.url_to_image(suggested_fragrances[2][5]))
+                        with details:
+                            st.write("**BRAND:** " + suggested_fragrances[2][6])
+                            st.write("**BASE NOTES:** " + suggested_fragrances[2][1])
+                            st.write("**SECONDARY NOTES:** " + suggested_fragrances[2][2])
+                            st.write("**GENDER:** " + suggested_fragrances[2][3])
+                            st.write("**AGE:** " + suggested_fragrances[2][4])
+                    except Exception as e:
+                        print(e)
+                        st.error("Could not load fragrance")
+
+                progress_bar.progress(100)
