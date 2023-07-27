84e915141e6d4b6ba6fc8c2c036632242fe842dd
Anuranjan M B <anuranjan.m.b@gmail.com>
2023-07-27 20:37:52 +0530

    Add files via upload

---
+++ pinecone_connection.py
@@ -0,0 +1,33 @@
+#####-----importing neccessary libraries-----#####
+
+import pinecone
+from streamlit.connections import ExperimentalBaseConnection
+from pinecone.core.client.model.query_response import QueryResponse 
+
+#####-----main connection clas-----#####
+
+class PineconeConnection(ExperimentalBaseConnection[pinecone.Index]):
+
+    def _connect(self, **kwargs) -> pinecone.Index:
+        pinecone.init(
+            api_key = self._secrets["PINECONE_API_KEY"],
+            environment = self._secrets["PINECONE_ENVIRONMENT"]
+            )
+        index = pinecone.Index(self._secrets["index_name"])
+        return index
+    
+    def cursor(self) -> pinecone.Index:
+        if hasattr(self, "_instance") and self._instance:
+            return self._instance
+        self._instance = self._connect()
+        return self._instance
+    
+    def query(self, query: list, n: int, ttl: int = 3600, **kwargs) -> QueryResponse:
+        @cache_data(ttl=ttl)
+        def _query(query: list, n: int, **kwargs) -> QueryResponse:
+            cursor = self.cursor()
+            result = cursor.query([query], top_k=n, include_metadata=True)
+            return result
+
+        return _query(query, n, **kwargs)
+        
